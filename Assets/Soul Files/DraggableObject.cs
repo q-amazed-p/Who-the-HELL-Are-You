3f83af8ffea6dfc782e7f24010eb5ab6885c6f50
q-amazed-p <qamazedp@gmail.com>
2025-08-01 00:49:33 +0200

    Draggable Soul File
    Simple object that becomes crudely highlighted when hovered and can be dragged around.

---
+++ Assets/Soul Files/DraggableObject.cs
@@ -0,0 +1,56 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DraggableObject : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler, IPointerMoveHandler
+{
+    Vector3 holderOffset;
+
+    bool isHeld;
+
+    public void OnPointerEnter(PointerEventData pointerData) 
+    {
+        GetComponent<Image>().color += new Color(0.05f, 0.05f, 0.05f, 0);
+    }
+
+    public void OnPointerExit(PointerEventData pointerData) 
+    {
+        GetComponent<Image>().color -= new  Color(0.05f, 0.05f, 0.05f, 0);
+        isHeld = false;
+
+    }
+
+    public void OnPointerDown(PointerEventData pointerData) 
+    {
+        holderOffset = transform.position - pointerData.pointerCurrentRaycast.worldPosition;
+        isHeld = true;
+        
+    }
+
+    public void OnPointerUp(PointerEventData pointerData)
+    {
+        isHeld = false;
+    }
+
+    public void OnPointerMove(PointerEventData pointerData) 
+    {
+        if (isHeld && pointerData.pointerCurrentRaycast.worldPosition != Vector3.zero)
+        {
+            transform.position = pointerData.pointerCurrentRaycast.worldPosition + holderOffset;
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        holderOffset = Vector3.zero;
+        isHeld = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
