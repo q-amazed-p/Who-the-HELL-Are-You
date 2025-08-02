aa1440c4e542377a87ba2bb28ebc35e022ce0e19
q-amazed-p <qamazedp@gmail.com>
2025-08-03 00:04:02 +0200

    Scales Implemented and Animated

---
+++ Assets/HellScale/Tilter.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using System.Collections;
+
+public class Tilter : MonoBehaviour
+{
+    [SerializeField] Quaternion rightExtreme;
+    [SerializeField] Quaternion leftExtreme;
+
+    [SerializeField] RectTransform rightPanT;
+    [SerializeField] RectTransform leftPanT;
+
+    Coroutine tilterator;
+
+    public void LoadRight() 
+    {
+        if (tilterator != null) StopCoroutine(tilterator);
+        tilterator = StartCoroutine(Tilterator(TiltDirection.right));
+    }
+
+    public void LoadLeft() 
+    {
+        if (tilterator != null) StopCoroutine(tilterator);
+        tilterator = StartCoroutine(Tilterator(TiltDirection.left));
+    }
+
+    public void Free()
+    {
+        if (tilterator != null) StopCoroutine(tilterator);
+        tilterator = StartCoroutine(Tilterator(TiltDirection.balanced));
+    }
+
+    IEnumerator Tilterator(TiltDirection tiltDirection) 
+    {
+        Quaternion startingAngle = transform.rotation;
+
+        Quaternion targetAngle = Quaternion.identity;
+        switch (tiltDirection) 
+        {
+            case TiltDirection.balanced:
+                break;
+            case TiltDirection.right:
+                targetAngle = rightExtreme;
+                break;
+            case TiltDirection.left:
+                targetAngle = leftExtreme;
+                break;
+        } 
+
+        for(float i = 0; i < 1;  i += Time.deltaTime) 
+        {
+            float dynamisedI = -0.5f*Mathf.Cos(10*i/Mathf.PI) + 0.5f;
+            Debug.Log("dynamisedI = " + dynamisedI);
+            transform.rotation = Quaternion.Lerp(startingAngle, targetAngle, dynamisedI);
+            Debug.Log("new rotation = " + Quaternion.Lerp(startingAngle, targetAngle, dynamisedI));
+
+            rightPanT.rotation = Quaternion.identity;
+            leftPanT.rotation = Quaternion.identity;
+
+            yield return null;
+        }
+    }
+
+}
+
+public enum TiltDirection 
+{ 
+    balanced = 0,
+    right = 1,
+    left = 2
+}
+
